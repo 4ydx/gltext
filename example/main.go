5566a741c4421433807c3670da876d338f5deaad
Unknown <danil@dumk.in>
2018-01-02 11:36:13 +0400

    Ooops

--- example/main.go
+++ example/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/dumkin/gltext"
+	"github.com/4ydx/gltext"
 	"github.com/go-gl/gl/v4.5-core/gl"
 	"github.com/go-gl/glfw/v3.2/glfw"
 	"github.com/go-gl/mathgl/mgl32"
